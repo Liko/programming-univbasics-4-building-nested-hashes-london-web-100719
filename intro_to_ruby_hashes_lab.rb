3a96d25ca8adfe5481e95e4cdd72388530708c27
Li Xie <li_xie1992@hotmail.com>
2019-09-09 05:01:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -77,19 +77,16 @@ def monopoly_with_fourth_tier
         {
           reading_railroad: 
           {
-            "mortage_value": "$100"
+            mortage_value: "$100"
           },
           pennsylvania_railroad: 
           {
-            mortage_value: "$200"
           },
           b_and_o_railroad: 
           {
-            mortage_value: "$400"
           },
           shortline_railroad: 
           {
-            mortage_value: "$800"
           }
         }
       }
