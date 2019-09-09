25685d63bd18034379d598762312ed53e3b89f3c
Li Xie <li_xie1992@hotmail.com>
2019-09-09 04:54:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,7 +39,7 @@ def monopoly_with_third_tier
       {
         reading_railroad: {},
         pennsylvania_railroad: {},
-        name3: "three",
+        :b_and_o_railroad: "three",
         name4: "four"
       }
     }
