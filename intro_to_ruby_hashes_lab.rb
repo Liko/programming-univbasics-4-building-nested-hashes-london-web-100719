14af267737f8ba09f65a362ddc40ecab235a6c90
Li Xie <li_xie1992@hotmail.com>
2019-09-09 04:59:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,33 +25,33 @@ def monopoly_with_third_tier
   # complexity.
   
     hash = 
-  { railroads:
-    {
-      pieces: 4,
-      rent_in_dollars: 
+    { railroads:
       {
-        one_piece_owned: 25,
-        two_pieces_owned: 50,
-        three_pieces_owned: 100,
-        four_pieces_owned: 200
-      },
-      names:
-      {
-        reading_railroad: 
+        pieces: 4,
+        rent_in_dollars: 
         {
+          one_piece_owned: 25,
+          two_pieces_owned: 50,
+          three_pieces_owned: 100,
+          four_pieces_owned: 200
         },
-        pennsylvania_railroad: 
+        names:
         {
-        },
-        b_and_o_railroad: 
-        {
-        },
-        shortline_railroad: 
-        {
+          reading_railroad: 
+          {
+          },
+          pennsylvania_railroad: 
+          {
+          },
+          b_and_o_railroad: 
+          {
+          },
+          shortline_railroad: 
+          {
+          }
         }
       }
     }
-  }
 end
 
 def monopoly_with_fourth_tier
